2253a20632b59505d8881515a1e3b33cc275cc4a
Henry Vega <hp.vega@hotmail.com>
2026-01-27 20:15:28 -0500

    add enum for order status and implement interfaces for identifiable and describable entities

--- ejemplos/03_poo_81.php
+++ ejemplos/03_poo_81.php
@@ -46,28 +46,74 @@ interface Describible
 # CLASE PEDIDO que implementa las interfaces
 class Pedido implements Identificable, Describible
 {
-    public int $id;
-    public string $nombre;
-    public string $fechaCreacion;
+    public readonly int $id;
+    public readonly string $nombre;
+    public readonly string $fechaCreacion;
     private EstadoPedido $estado;
     private array $productos = [];
 
     public function __construct(
-        public string $cliente,
-        $estado = EstadoPedido::Pendiente
+        public readonly string $cliente, // Promoción de propiedad solo lectura
+                               $estado = EstadoPedido::Pendiente
     )
     {
         $this->id = random_int(1000, 9999);
         $this->fechaCreacion = date("Y-m-d H:i:s");
+        $this->estado = $estado;
     }
 
     public function getId(): int
     {
-
+        return $this->id;
     }
 
     public function getDescripcion(): string
     {
+//        return "Pedido #" . $this->id . " para " . $this->cliente;
+        return "Pedido #{$this->id} para {$this->cliente}";
+    }
 
+    public function getEstado(): EstadoPedido
+    {
+        return $this->estado;
+    }
+
+    public function cambiarEstado(EstadoPedido $nuevoEstado): void
+    {
+        echo "Cambiando estado de {$this->estado->name} a {$nuevoEstado->name}" . PHP_EOL;
+        $this->estado = $nuevoEstado;
+    }
+
+    public function agregarProductos(string $producto, float $precio): void
+    {
+//        array_push($this->productos, ['nombre' => $producto, 'precio' => $precio]);
+        $this->productos[] = ['nombre' => $producto, 'precio' => $precio];
+    }
+
+    public function getTotal(): float
+    {
+        return array_sum(array_column($this->productos, 'precio'));
     }
 }
+
+# EJEMPLO DE USO
+
+$pedido = new Pedido("Juan Pérez");
+
+echo "Id del pedido: {$pedido->getId()}" . PHP_EOL;
+echo "Cliente: {$pedido->cliente}" . PHP_EOL;
+echo "Feche de creación: {$pedido->fechaCreacion}" . PHP_EOL;
+
+// Agregar productos
+
+$pedido->agregarProductos("Laptop",1200);
+$pedido->agregarProductos("Mouse",25);
+$pedido->agregarProductos("Teclado",75.50);
+
+echo "Total del pedido: $ {$pedido->getTotal()}" . PHP_EOL;
+
+$pedido->cambiarEstado(EstadoPedido::Enviado);
+$pedido->cambiarEstado(EstadoPedido::Entregado);
+
+//$pedido->id = 5000; // Modificando el id del pedido
+echo "Id del pedido: {$pedido->getId()}" . PHP_EOL;
